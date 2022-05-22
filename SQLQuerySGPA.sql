3f4e2bf5383b2a795bc05e689ed09d0740132592
Juanpablo000 <57875962+Juanpablo000@users.noreply.github.com>
2022-05-22 15:12:02 -0500

    Update SQLQuerySGPA.sql

--- SQLQuerySGPA.sql
+++ SQLQuerySGPA.sql
@@ -1,192 +1,195 @@
+use [master]
+GO
+
+CREATE DATABASE [BDSGA]
+GO
+
+USE BDSGA
+GO
+
 CREATE TABLE Articulos (
 	IdArticulo INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
 	NombreArticulo VARCHAR(40) NOT NULL,
 	PrecioVenta int NOT NULL,
 	PrecioUnitario int NOT NULL,
 	Cantidad int NOT NULL,
-	IdCategoria int NOT NULL,
+	NombreCategoria VARCHAR(40) NOT NULL,
 	IdProeevedor int NOT NULL,
 );
+GO
+
+/*Num Fact, Id Cliente, Empleado, Fecha, Forma Pago, Total, IVA*/
 
 CREATE TABLE Facturacion (
-	IdFactura INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-	IdPago INT NOT NULL,
-	Fecha DATE NOT NULL,
+	NumeroFactura VARCHAR(40) PRIMARY KEY  NOT NULL,
 	IdCliente INT NOT NULL,
+	Empleado VARCHAR(40) NOT NULL,
+	Fecha DATE NOT NULL,
+	FormaPago VARCHAR(40) NOT NULL,
+	Total INT NOT NULL,
+	Iva INT NOT NULL
 );
+GO
 
-CREATE TABLE Categorias (
-	IdCategoria INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-	NombreCategoria VARCHAR(40) NOT NULL,
-);
-
-CREATE TABLE FormasPago (
-	IdPago INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-	NombrePago VARCHAR(40) NOT NULL,
-);
 
 CREATE TABLE DetalleFactura (
 	IdDetalleFac INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-	IdFactura INT NOT NULL,
+	NumeroFactura VARCHAR(40) NOT NULL,
+	Descripcion VARCHAR(40) NOT NULL,
 	Cantidad INT NOT NULL,
-	IdProducto INT NOT NULL,
+	TotalDetalle INT NOT NULL
 );
+GO
 
 CREATE TABLE Clientes (
 	IdCliente INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-	IdTipoDocumento INT NOT NULL,
+	TipoDoc VARCHAR(40) NOT NULL,
 	NumeroDocumento INT NOT NULL,
 	Nombres VARCHAR(40) NOT NULL,
 	Apellidos VARCHAR(40) NOT NULL,
 	Direccion VARCHAR(40) NOT NULL,
 	Telefono BIGINT NOT NULL,
+	Ciudad VARCHAR (40) NOT NULL
 );
+GO
 
 CREATE TABLE Proeevedores (
 	IdProeevedor INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-	IdTipoDocumento INT NOT NULL,
+	TipoDoc VARCHAR(40) NOT NULL,
 	NumeroDocumento INT NOT NULL,
 	Nombres VARCHAR(40) NOT NULL,
 	Apellidos VARCHAR(40) NOT NULL,
-	IdEmpresa INT NOT NULL,
-);
-
-CREATE TABLE EmpresasProeevedoras (
-	IdEmpresa INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-	Nombre VARCHAR(100) NOT NULL,
+	NombreComercial VARCHAR(100) NOT NULL,
 	Direccion VARCHAR(40) NOT NULL,
-	Ciudad VARCHAR(40) NOT NULL,
-	Telefono BIGINT NOT NULL,
+	Ciudad VARCHAR (40) NOT NULL,
+	Telefono BIGINT NOT NULL
 );
+GO
 
-drop table TipoDocumento
-
-CREATE TABLE TipoDocumento (
-	IdTipoDocumento INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-	NomDocumento VARCHAR(40) NOT NULL,
+CREATE TABLE Devoluciones (
+	IdDevolucion INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
+	NumeroFactura VARCHAR(40) NOT NULL,
+	IdArticulo INT NOT NULL,
+	Cantidad INT NOT NULL,
+	Motivo VARCHAR(40) NOT NULL,
+	Fecha DATE NOT NULL
 );
+GO
+
 
 
 /*Creacion de la llave foranea para la tabla de articulos*/
 ALTER TABLE Articulos
-ADD CONSTRAINT FK_Categoria
-FOREIGN KEY (IdCategoria) 
-REFERENCES Categorias (IdCategoria)
-
-ALTER TABLE Articulos
 ADD CONSTRAINT FK_Proeevedor
 FOREIGN KEY (IdProeevedor) 
 REFERENCES Proeevedores (IdProeevedor)
-
-/*Creacion de la llave foranea para la tabla de facturaci�n*/
-ALTER TABLE Facturacion
-ADD CONSTRAINT FK_Pago
-FOREIGN KEY (IdPago) 
-REFERENCES FormasPago (IdPago)
-
-
+GO
+/*Creacion de la llave foranea para la tabla de facturaci n*/
 ALTER TABLE Facturacion
 ADD CONSTRAINT FK_Cliente
 FOREIGN KEY (IdCliente) 
 REFERENCES Clientes (IdCliente)
-
+GO
 
 /*Creacion de las llaves foraneas para la tabla de detalle factura*/
 ALTER TABLE DetalleFactura
 ADD CONSTRAINT FK_Factura
-FOREIGN KEY (IdFactura) 
-REFERENCES Facturacion (IdFactura)
+FOREIGN KEY (NumeroFactura) 
+REFERENCES Facturacion (NumeroFactura)
+GO
 
+/*Devoluciones*/
+ALTER TABLE Devoluciones
+ADD CONSTRAINT FK_DevolucionNFACT
+FOREIGN KEY (NumeroFactura) 
+REFERENCES Facturacion (NumeroFactura)
+GO
 
-ALTER TABLE DetalleFactura
-ADD CONSTRAINT FK_Articulo
-FOREIGN KEY (IdProducto) 
+ALTER TABLE Devoluciones
+ADD CONSTRAINT FK_DevolucionArticulo
+FOREIGN KEY (IdArticulo) 
 REFERENCES Articulos (IdArticulo)
+GO
 
-/*Creacion de las llaves foraneas para la tabla de proeevedores*/
-ALTER TABLE Proeevedores
-ADD CONSTRAINT FK_Empresa
-FOREIGN KEY (IdEmpresa) 
-REFERENCES EmpresasProeevedoras (IdEmpresa)
-
-/*Clientes*/
-ALTER TABLE Clientes
-ADD CONSTRAINT FK_Documento
-FOREIGN KEY (IdTipoDocumento) 
-REFERENCES TipoDocumento (IdTipoDocumento)
-
-select * from Clientes
-
-CREATE PROCEDURE SP_ActualizarClientes
-(
-@IdDoc as int,
-@NumeroDoc as BigInt,
-@Nombres as varchar(40),
-@Apellidos as varchar(40),
-@Direccion as varchar(40),
-@Telefono as BigInt
+CREATE PROCEDURE SP_RegistrarClientes(	
+	@TipoDoc as varchar(40),
+	@NumeroDoc as BigInt,	
+	@Nombres as varchar(40),
+	@Apellidos as varchar(40),
+	@Direccion as varchar(40),
+	@Telefono as BigInt,
+	@Ciudad as varchar(40)
+	
 )
 AS 
 BEGIN
-	INSERT INTO Clientes (IdTipoDocumento, NumeroDocumento, Nombres, Apellidos, Direccion, Telefono)
-	VALUES (@IdDoc, @NumeroDoc, @Nombres,@Apellidos,@Direccion, @Telefono);
+	INSERT INTO Clientes (TipoDoc, NumeroDocumento, Nombres, Apellidos, Direccion, Telefono, Ciudad)
+	VALUES (@TipoDoc, @NumeroDoc, @Nombres,@Apellidos,@Direccion, @Telefono, @Ciudad);
 END
-
-EXECUTE SP_ActualizarClientes 1,1018208734,'Sebastian', 'Uzuga Quintero', 'Carrera 124', 3124356743
+GO
+/*EXECUTE SP_ActualizarClientes 1,1018208734,'Sebastian', 'Uzuga Quintero', 'Carrera 124', 3124356743
 EXECUTE SP_ActualizarClientes 1,1018208735,'Sara Estefania', 'Ruiz Florez', 'Diagonal 123', 3156656743
 
 select * from Clientes
 select * from Proeevedores
+*/
 
-CREATE PROCEDURE SP_ActualizarProeevedores
+CREATE PROCEDURE SP_RegistrarProeevedores
 (
-@NumeroDoc as BigInt,
-@IdDoc as int,
-@Nombres as varchar(40),
-@Apellidos as varchar(40),
-@IdEmpresa as int
+	@TipoDoc as varchar(40),
+	@NumeroDoc as BigInt,	
+	@Nombres as varchar(40),
+	@Apellidos as varchar(40),
+	@NombreComercial as varchar(100),
+	@Direccion as varchar(40),
+	@Ciudad as varchar(40),
+	@Telefono as BigInt
 )
 AS 
 BEGIN
-	INSERT INTO Proeevedores (IdTipoDocumento, NumeroDocumento, Nombres, Apellidos, IdEmpresa)
-	VALUES (@IdDoc, @NumeroDoc, @Nombres,@Apellidos,@IdEmpresa);
+	INSERT INTO Proeevedores (TipoDoc, NumeroDocumento, Nombres, Apellidos, NombreComercial, Direccion, Ciudad, Telefono)
+	VALUES (@tipoDoc , @NumeroDoc, @Nombres,@Apellidos,@NombreComercial,@Direccion,@Ciudad,@Telefono);
 END
-
-EXECUTE SP_ActualizarProeevedores 1543789432,2,'David', 'Mu�oz Gomez', 5
+GO
+/*
+EXECUTE SP_ActualizarProeevedores 1543789432,2,'David', 'Mu oz Gomez', 5
 
 select * from Proeevedores
 
 select * from EmpresasProeevedoras
 
 select * from Clientes
+*/
 
 
 CREATE PROCEDURE SP_RegistarArticulos
 (
-@NombreArticulo as varchar(40),
-@PrecioVenta as int,
-@PrecioUnitario as int,
-@Cantidad as varchar(40),
-@IdCategoria as varchar(40),
-@IdProeevedor as int
+	@NombreArticulo as varchar(40),
+	@PrecioVenta as int,
+	@PrecioUnitario as int,
+	@Cantidad as varchar(40),
+	@NombreCategoria as varchar(40),
+	@IdProeevedor as int
 )
 AS 
 BEGIN
-	INSERT INTO Articulos(NombreArticulo, PrecioVenta, PrecioUnitario, Cantidad, IdCategoria, IdProeevedor)
-	VALUES (@NombreArticulo, @PrecioVenta, @PrecioUnitario,@Cantidad,@IdCategoria,@IdProeevedor);
+	INSERT INTO Articulos(NombreArticulo, PrecioVenta, PrecioUnitario, Cantidad, NombreCategoria, IdProeevedor)
+	VALUES (@NombreArticulo, @PrecioVenta, @PrecioUnitario,@Cantidad,@NombreCategoria,@IdProeevedor);
 END
-
+GO
+/*
 EXECUTE SP_RegistarArticulos 'Salchicha de cerdo', 5500, 3000, 100, 1, 1
 
 Select * from Categorias
 Select * from Proeevedores
 
 Select * from Articulos
+*/
 
 CREATE PROCEDURE SP_ActualizarStock
 (
-@ID_Articulo as int,
-@Cantidad as varchar(40)
+	@ID_Articulo as int,
+	@Cantidad as varchar(40)
 )
 AS 
 BEGIN
@@ -194,8 +197,61 @@ BEGIN
 	SET Cantidad=@Cantidad
 	WHERE IdArticulo=@ID_Articulo
 END
-
+GO
+/*
 EXECUTE SP_ActualizarStock 1,110
+*/
 
 
-Select * from Articulos
+CREATE LOGIN AdminBDSGA WITH PASSWORD = '1234' /* centralizacionCXP en Hexadecimal  */
+
+CREATE USER AdminBDSGA FOR LOGIN AdminBDSGA
+
+ALTER ROLE db_owner ADD member AdminBDSGA
+GO
+
+
+CREATE PROCEDURE SP_RegistrarDetalleFactura(	
+	@NumFactura as varchar(40),
+	@Descripcion as varchar(40),
+	@Cantidad as int,
+	@TotalDetalle as int
+)
+AS 
+BEGIN
+	INSERT INTO DetalleFactura(NumeroFactura, Descripcion, Cantidad, TotalDetalle)
+	VALUES (@NumFactura, @Descripcion, @Cantidad,@TotalDetalle);
+END
+GO
+
+
+
+CREATE PROCEDURE SP_RegistrarFactura(
+	@NumFactura as varchar(40),
+	@IdCliente as int,	
+	@Empleado as varchar(40),
+	@Fecha as date,
+	@FormaPago as varchar(40),
+	@Total as int,
+	@Iva as int
+)
+AS 
+BEGIN
+	INSERT INTO Facturacion(NumeroFactura, IdCliente, Empleado, Fecha, FormaPago, Total, Iva)
+	VALUES (@NumFactura, @IdCliente, @Empleado,@Fecha,@FormaPago, @Total, @Iva);
+END
+GO
+
+CREATE PROCEDURE SP_RegistrarDevoluciones(	
+	@NumFactura as varchar(40),
+	@IdArticulo as int,	
+	@Cantidad as int,
+	@Motivo as varchar(40),
+	@Fecha as date
+)
+AS 
+BEGIN
+	INSERT INTO Devoluciones(NumeroFactura, IdArticulo, Cantidad, Motivo, Fecha)
+	VALUES (@NumFactura, @IdArticulo, @Cantidad,@Motivo,@Fecha);
+END
+GO
